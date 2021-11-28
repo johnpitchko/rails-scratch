f2d1ad3338662e80919c9f932747e9470341a97d
John Pitchko <john.pitchko@icloud.com>
2021-11-28 08:36:08 -0700

    Serve static pages from root

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -3,4 +3,7 @@
 HighVoltage.configure do |config|
   # Set the site root page to /app/views/pages/index.html.erb
   config.home_page = 'index'
+
+  # Top-level routes: eliminate the need to specify '.../pages'...' folder in URL
+  config.route_drawer = HighVoltage::RouteDrawers::Root
 end
